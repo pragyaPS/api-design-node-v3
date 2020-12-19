66aca9434b211dd1376023a2e445c22b6a21c83c
Pragya Singh <pragyasingh991@gmail.com>
2020-12-20 02:05:04 +1100

    Lesson 1 activities

--- src/server.js
+++ src/server.js
@@ -12,4 +12,32 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+const log = (req, res, next) => {
+  console.log('logging')
+   next();
+}
+const router = express.Router();
+router.get('/me', (req, res) => {
+  res.send({message: 'this is from router me'})
+})
+app.use('/api', router)
+
+app.get('/', log,  (req, res) => {
+  res.send({name: "beautiful Pragya", age: 'sweet 29'});
+});
+app.post('/', (req, res) => {
+  console.log('req', req.body);
+  res.send({message: `message is: ${req.body.name}`});
+})
+app.get('/data/:id', (req, res) => {
+  console.log('req.body', req.body);
+  res.send({name: `${req.body} is response`})
+})
+
+
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log("server running on port 3000");
+  });
+}
