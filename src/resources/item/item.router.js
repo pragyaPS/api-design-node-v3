197bc20c2a9766132cb01d0d1a2ae3f998fca431
Pragya Singh <pragyasingh991@gmail.com>
2020-12-20 23:29:57 +1100

    Finished lesson 2 exercise

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -2,4 +2,20 @@ import { Router } from 'express'
 
 const router = Router()
 
+
+
+router.route('/').get(() => {
+    console.log('/ get route')
+}).post(() => {
+    console.log('/ post route')
+})
+
+router.route('/:id').get(() => {
+    console.log('/:id get')
+})
+.delete(() => {
+    console.log('/:id delete')
+}).put(() => {
+    console.log('/:id post')
+})
 export default router
